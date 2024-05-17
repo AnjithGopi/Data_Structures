96b799e4fdd040f4e7aa1b79da4b8309b0e0279e
AnjithGopi <ng.anjith444@gmail.com>
2024-05-17 12:53:38 +0530

    Linear search

---
+++ Searching/LinearSearch.js
@@ -0,0 +1,33 @@
+
+// Searching a target  element using linear search
+
+
+function linearSearch(arr,tar){
+
+
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]===tar){
+            return i
+        }
+    }
+
+
+
+
+}
+
+
+
+const array=[1,2,3,4,5,6]
+
+const target=4  // output=3
+
+const fn=linearSearch(array,target)
+
+console.log(fn)
+
+
+
+
+
+
