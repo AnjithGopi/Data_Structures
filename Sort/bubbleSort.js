c1840ef4af297157d89a1dbe09ceff4125a6e710
AnjithGopi <ng.anjith444@gmail.com>
2024-06-15 14:58:18 +0530

    bubble

--- Sort/bubbleSort.js
+++ Sort/bubbleSort.js
@@ -1,6 +1,8 @@
 
 
 
+// Sorting the array in the increasing order , using bubble sort
+
 
 function bubbleSort(arr){
 
@@ -35,3 +37,42 @@ function bubbleSort(arr){
 const arr= new Array(-5,-1,-8,0,2,-7,5,6,4,9)
 
 console.log(bubbleSort(arr))
+
+
+
+
+//   Sorting the array in the decreasing order using bubble sort
+
+
+
+
+
+
+
+function bubbleSortDecreasing(arr){
+
+    let swapped
+
+    do {
+
+
+        swapped=false
+        for(let i=0;i<arr.length-1;i++){
+            if(arr[i]<arr[i+1]){
+                let temp=arr[i]
+                arr[i]=arr[i+1]
+                arr[i+1]=temp
+                swapped=true
+            }
+        }
+        
+    } while (swapped);
+
+
+
+    return arr
+
+}
+
+
+console.log(bubbleSortDecreasing(arr))
