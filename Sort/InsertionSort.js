cfde0e8951a869385a7fabc2714ce9a6c350577f
AnjithGopi <ng.anjith444@gmail.com>
2024-06-18 18:16:52 +0530

    insertion sort

--- Sort/InsertionSort.js
+++ Sort/InsertionSort.js
@@ -5,3 +5,40 @@
 
 
 
+function insertionSort(arr){
+
+    for(let i=1;i<arr.length;i++){
+
+        let key= arr[i]
+
+        let j=i-1
+
+
+        while(j>=0 && arr[j]>key){
+
+            arr[j+1]=arr[j]
+
+
+            j=j-1
+
+        }
+
+        arr[j+1]=key
+    }
+
+
+    return arr
+
+
+
+
+}
+
+
+
+
+
+const array= new Array(10,4,5,2,3,7,6,8)
+
+
+console.log(insertionSort(array))
