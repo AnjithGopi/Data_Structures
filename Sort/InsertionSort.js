49d5072e55f2087f2f94aa3ab15c2fafc85eb031
AnjithGopi <ng.anjith444@gmail.com>
2024-06-19 12:22:52 +0530

    insertion

--- Sort/InsertionSort.js
+++ Sort/InsertionSort.js
@@ -58,9 +58,62 @@ function insertionSortDescending(arr){
 
 
 
+// ascending order sort using insertion sort algorithms
+
+
+
+
+function ascending(arr){
+
+
+    for(let i=1;i<arr.length;i++){
+
+        let key= arr[i]
+
+
+        let j=i-1
+
+        while(j>=0 && arr[j]>key){
+
+            arr[j+1]=arr[j]
+
+            j--
+        }
+
+        arr[j+1]=key
+
+    }
+
+    return arr
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const array= new Array(10,4,5,2,3,7,6,8)
-
-
 console.log(insertionSort(array))
 console.log(insertionSortDescending(array))
+console.log(ascending(array))
