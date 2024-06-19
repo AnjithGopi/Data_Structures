ac2e69de46748ec80852962444bc880691cb104c
AnjithGopi <ng.anjith444@gmail.com>
2024-06-19 12:42:49 +0530

    sample

--- Sample.js
+++ Sample.js
@@ -32,6 +32,77 @@ function binarySearch(arr,tar){
 
 
 
+// sorting using bubble sort
+
+
+
+
+
+
+function bubble(arr){
+
+    let swapped
+
+
+    do {
+
+
+        swapped=false
+        for(let i=0;i<arr.length;i++){
+
+            if(arr[i]>arr[i+1]){
+                let temp=arr[i]
+                arr[i]=arr[i+1]
+                arr[i+1]=temp
+                swapped =true
+            }
+
+        
+
+        }
+        
+    } while (swapped);
+
+
+    return arr
+
+}
+
+
+// sorting the same array using insertion sort algorithm
+
+
+
+
+
+function insertion(arr){
+
+
+    for(let i=1;i<arr.length;i++){
+        let key=arr[i]
+
+        let j=i-1
+
+
+        while(j>=0 && arr[j]>key){
+
+            arr[j+1]=arr[j]
+
+            j--
+
+        }
+
+        arr[j+1]=key
+    }
+
+
+     return arr
+
+}
+
+
+
+
 
 
 
@@ -39,4 +110,11 @@ const arr= new Array(1,2,3,4,5)
 
 const tar= 4 
 
+
+console.log(bubble(arr))
+console.log(insertion(arr))
 console.log(binarySearch(arr,tar))
+
+
+
+
