0ff75050e7891655c7268d2ade501931c481841e
AnjithGopi <ng.anjith444@gmail.com>
2024-06-19 14:55:31 +0530

    quick sort

--- Sort/QuickSort.js
+++ Sort/QuickSort.js
@@ -36,9 +36,47 @@ function quickSort(arr){
 
 
 
+// sort the given array in non increasing order 
+
+
+
+function ascending(array){
+
+
+    if(array.length<2){
+        return array
+    }
+
+
+    let pivot= array[array.length-1]
+    let left=[]
+    let right=[]
+
+    for(let i=0;i<array.length-1;i++){
+
+        if(array[i]>pivot){
+
+          left.push(array[i])
+
+        }else{
+            right.push(array[i])
+        }
+
+    }
+
+
+    return [...ascending(left),pivot,...ascending(right)]
+
+
+
+}
+
+
+
 
 
 
 const array= new Array(10,12,11,3,0,-6,7,6)
 
 console.log(quickSort(array))
+console.log(ascending(array))
