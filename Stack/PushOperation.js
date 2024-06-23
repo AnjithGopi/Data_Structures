6b2771dbb814c1f369e289229361b5732ae706ae
AnjithGopi <ng.anjith444@gmail.com>
2024-06-23 23:47:48 +0530

    stack operations

--- Stack/PushOperation.js
+++ Stack/PushOperation.js
@@ -1,33 +1,41 @@
 
 
-
 class Stack{
 
 
     constructor(){
-
-
         this.item=[]
     }
 
 
-
     push(element){
-
         this.item.push(element)
     }
 
 
+    pop(){
+        if(this.item.length===0){
+              console.log("Stack is empty")
+            return
+        }
+
+
+        this.item.pop()
+
+
+    }
+
 
     display(){
-
 
         console.log(this.item)
     }
 }
 
 
-const stack = new Stack()
+
+
+const stack= new Stack()
 
 
 stack.push(10)
@@ -36,4 +44,6 @@ stack.push(30)
 stack.push(40)
 stack.push(50)
 
+stack.display()
+stack.pop()
 stack.display()
