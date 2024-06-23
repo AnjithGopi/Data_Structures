0d0a0018e9006b0a91eaf788038a3b260210cc01
AnjithGopi <ng.anjith444@gmail.com>
2024-06-23 23:20:44 +0530

    Stack

--- Stack/stack.js
+++ Stack/stack.js
@@ -1,3 +1,14 @@
 
 
-//
+// Stack is a sequential collection of elements that follows the last in first out principle 
+
+
+// the main operations involved with stack are
+
+
+//Push :-  Adding an element into the stack
+
+
+//Pop:= Removing an element from the stack
+
+
