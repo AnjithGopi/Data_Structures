e07b81ab1fcae4844563686e38841a93d8291a25
AnjithGopi <ng.anjith444@gmail.com>
2024-06-24 12:39:54 +0530

    sorting

--- Sort/QuickSort.js
+++ Sort/QuickSort.js
@@ -76,7 +76,46 @@ function ascending(array){
 
 
 
+function quick(arr){
+
+
+    if(arr.length<=1){
+        return arr
+    }
+
+
+    let pivot= arr[arr.length-1]
+
+    let left=[]
+    let right=[]
+
+    for(let i=0;i<arr.length-1;i++){
+
+        if(arr[i]<pivot){
+            left.push(arr[i])
+        }else{
+            right.push(arr[i])
+        }
+    }
+
+
+
+    return [...quick(left),pivot,...quick(right)]
+
+
+
+
+}
+
+
+
+
+
+
 const array= new Array(10,12,11,3,0,-6,7,6)
+
+
+console.log(quick(array))
 
 console.log(quickSort(array))
 console.log(ascending(array))
