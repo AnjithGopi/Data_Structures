e83ea01876a1a7faf99870b8f4fdbd8efc1978a1
AnjithGopi <ng.anjith444@gmail.com>
2024-07-08 15:36:29 +0530

    updated

--- Array/new.js
+++ Array/new.js
@@ -1,2 +1,29 @@
 
 
+
+
+
+function duplicate(array){
+
+
+    for(item of array){
+
+        if(array.indexOf(item)!==array.lastIndexOf(item)){
+
+            return item
+
+        }
+        
+    }
+
+    return "no duplicates"
+
+}
+
+
+
+const arr= new Array(110,20,30,40,100,40)
+
+console.log(duplicate(arr))
+
+ 
