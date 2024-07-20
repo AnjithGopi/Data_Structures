e5337b02f8cca3fd522b1bbd2cf4eb08d65d3d9c
AnjithGopi <ng.anjith444@gmail.com>
2024-07-20 15:16:31 +0530

    recursion

--- Recursion/fibonacci.js
+++ Recursion/fibonacci.js
@@ -5,46 +5,17 @@
 
 function fibonnacci(n){
 
-    if(n===0||n==1){
-        return n
-    }
+   if(n<=1){
+    return n
+   }
 
-    return fibonnacci(n-2)+fibonnacci(n-1)
-
+   return fibonnacci(n-1)+fibonnacci(n-2)
    
 
 }
 
-function print(count){
-
-    for(let i=0;i<count;i++){
-        console.log(fibonnacci(i))
-    }
-
-}
+ console.log(fibonnacci(6))
 
 
-
-
- print(6)
-
-
-
-
- ////////////////////
-
-
-
- function figo(limit,n1=0,n2=1,result=[]){
-    if(n1>limit){
-        return result
-    }
-
-    result.push(n1)
-    let next= n1+n2;
-    return figo(limit,n2,next,result)
- }
-
- console.log(figo(100));
 
  
