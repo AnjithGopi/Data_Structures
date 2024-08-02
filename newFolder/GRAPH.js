f44b5946aecf637978ccdf864dd5d866d7934e35
AnjithGopi <ng.anjith444@gmail.com>
2024-08-02 09:55:04 +0530

    graph added

---
+++ newFolder/GRAPH.js
@@ -0,0 +1,110 @@
+
+// implementation and operations in Graph datastructure
+
+
+class Graph{
+
+    constructor(){
+        this.adjacencyList={}
+    }
+
+
+
+    addVertex(vertex){
+
+        if(!this.adjacencyList[vertex]){
+            this.adjacencyList[vertex]= new Set()
+        }
+    }
+
+
+    addEdge(vertex1,vertex2){
+
+
+        if(!this.adjacencyList[vertex1]){
+            this.addVertex(vertex1)
+        }
+
+        if(!this.adjacencyList[vertex2]){
+
+            this.addVertex(vertex2)
+        }
+
+
+        this.adjacencyList[vertex1].add(vertex2)
+    }
+
+    display(){
+
+        for(let vertex in this.adjacencyList){
+
+            console.log(vertex+"-->"+[...this.adjacencyList[vertex]])
+
+        }
+
+       
+    }
+
+
+    bfs(start){
+
+        let queue=[start]
+        let visited=new Set([start])
+
+        while(queue.length>0){
+
+            let vertex=queue.shift()
+            console.log(vertex)
+
+            for(let neighbor of this.adjacencyList[vertex]){
+
+                if(!visited.has(neighbor)){
+                    visited.add(neighbor)
+                    queue.push(neighbor)
+                }
+            }
+        }
+    }
+
+
+    dfs(start){
+
+
+        let stack=[start]
+        let visited= new Set([start])
+
+
+        while(stack.length>0){
+
+            let vertex=stack.pop()
+            console.log(vertex)
+
+
+            for(let neighbor of this.adjacencyList[vertex]){
+
+                if(!visited.has(neighbor)){
+
+                    visited.add(neighbor)
+                    stack.push(neighbor)
+                }
+            }
+        }
+    }
+}
+
+
+
+
+const graph= new Graph()
+
+
+graph.addVertex("A")
+graph.addVertex("B")
+graph.addVertex("C")
+
+graph.addEdge("A","B")
+graph.addEdge("B","C")
+
+graph.display()
+graph.bfs("A")
+graph.dfs("A")
